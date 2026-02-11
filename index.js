196db436ff32242eb90dafdd939d167a1b33e46e
Bapi Biswas <107844015+bapibiswas100808@users.noreply.github.com>
2026-02-11 10:57:34 +0600

    Update index.js

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const jobCollections = client.db("tutorHub").collection("allJobs");
     const tutorCollections = client.db("tutorHub").collection("allTutors");
